acaf78da782d655f3be80d5e93ac851efc9c5fcd
unknown <kinney.kyle16@gmail.com>
2020-06-24 17:29:53 -0500

    Poverty/Healthcare Chart complete with labels

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("#chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -24,12 +24,72 @@ var chartGroup = svg.append("g")
 d3.csv("assets/data/data.csv").then(function(stateData) {
   console.log(stateData);
   
-  stateData.forEach(function(data) {
-    //state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,healthcare,healthcareLow,healthcareHigh,obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh
-    // data.state = +data.state;
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;
+    stateData.forEach(function(data) {
+      //state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,healthcare,healthcareLow,healthcareHigh,obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh
+      // data.state = +data.state;
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
 
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(stateData, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(stateData, d => d.healthcare)])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Bubbles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "9")
+    .attr("fill", "gray")
+    .attr("opacity", ".5");
+
+    svg.selectAll(".stateAbbr")
+       .data(stateData)
+       .enter()
+       .append("text")
+       .classed("stateAbbr", true)
+       .text(d => d.abbr)
+       .attr('dy', 3.5)
+       .attr('dx', -5.5)
+       .attr('font-size', '9px')
+       .attr("x", d => xLinearScale(d.poverty))
+       .attr("y", d => yLinearScale(d.healthcare))
+       .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lack Healthcare (%)");
     
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+  }).catch(function(error) {
+    console.log(error);
+
   });
-});
